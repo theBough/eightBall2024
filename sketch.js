e54f34d31319c95193f89f0b9b6ced1eea573934
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-09 09:51:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let myFont
+
 function setup() {
   createCanvas(400, 400);
+  myFont = loadFont("tilt.ttf")
+  
 }
 
 function draw() {
-  background(220);
+  background(225);
+  textFont(myFont);
+  textSize(25)
+  text("Eight Ball", 150, 20)
 }
