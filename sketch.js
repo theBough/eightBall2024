351cc52761fcc1edb383113c1c282d9111ad365c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-10 10:36:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 let myFont;
 let myInput;
+let myButton
 
 function setup() {
   createCanvas(400, 400);
   myFont = loadFont("tilt.ttf");
   myInput =createInput();
-  myInput.position(110,75)
-}
+  myInput.position(110,75);
+  myButton = createButton("press me")
+  myButton.position(110,100)
+  myButton.style("cursor","pointer");
+}//end Setup
 function draw() {
   background("#C1121F");
   textFont(myFont);
@@ -15,4 +19,4 @@ function draw() {
   text("Eight Ball", 150, 20);
   textSize(15);
   text("Ask Me a Question", 137, 50);
-}
+}//end Draw
