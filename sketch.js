163413e8058c9a924c9f51b895ffd9d7f036c393
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-10 10:23:23 -0500

    Has an input box

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
-let myFont
+let myFont;
+let myInput;
 
 function setup() {
   createCanvas(400, 400);
-  myFont = loadFont("tilt.ttf")
-  
+  myFont = loadFont("tilt.ttf");
+  myInput =createInput();
+  myInput.position(110,75)
 }
-
 function draw() {
-  background(225);
+  background("#C1121F");
   textFont(myFont);
-  textSize(25)
-  text("Eight Ball", 150, 20)
+  textSize(25);
+  fill("#FDF0D5");
+  text("Eight Ball", 150, 20);
+  textSize(15);
+  text("Ask Me a Question", 137, 50);
 }
