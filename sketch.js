1798fe5c968b693a57b0d59ec2e7d7fd1b107fa0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-16 09:05:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,24 +3,29 @@ let myInput;
 let myButton;
 let answer;
 let rndNum;
-let theirQuestion
+let theirQuestion;
 let img;
+let imgW;
+let imgH;
 
 function setup() {
   //The setup is similar to:
   //"when Green Flag is clicked in Scratch"
   createCanvas(400, 400);
   myFont = loadFont("tilt.ttf");
-  img = loadImage("bus.jpg")
+  
+  img = loadImage("eightBallGood.png")
   myInput =createInput();
   myInput.position(110,75);
   myButton = createButton("press me")
   myButton.position(110,100)
   myButton.style("cursor","pointer");
   myButton.style("background-color","#003049")
-  myButton.mousePressed(askQuestion);
+  myButton.mousePressed(thinking);
   answer = "My Answer will Show here."
   theirQuestion = ">> "
+  imgH = 100;
+  imgW = 100;
 }//end Setup
 function draw() {
   //THis is similar to a Forever loop
@@ -34,15 +39,21 @@ function draw() {
   text("Ask Me a Question", 137, 50);
   text(answer, 110,180)
   text(theirQuestion, 110, 150)
+  img.resize(imgW,imgH)
   image(img , 110 , 190)
  
 }//end Draw
-
+function thinking(){
+  img = loadImage("thinking.jpg")
+  setTimeout(askQuestion, 5000)
+}
 function askQuestion(){
   theirQuestion  = ">> " + myInput.value()
   makeRandomNumber();
   if(rndNum == 1){
     answer = "That's a Hard No!!!"
+    imgW = 150;
+    imgH = 150;
     img = loadImage("hardNo.jpg")
    
   }//end if
